b9a94b98858011b44024624032e71b4c858bb458
deannaliu <deanna.mei.liu@gmail.com>
2025-03-09 23:39:44 -0400

    calculator

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import Header from "./components/Header.jsx"
+import UserInput from "./components/UserInput.jsx"
+import Output from "./components/Output.jsx"
+
+import { useState } from "react";
+
+function App() {
+  // states of each input are in an object
+  const [inputs, setInputState] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+});
+
+const validInputs = inputs.duration > 0;
+
+function setInputHandler(inputIdentifier, newValue) {
+  setInputState((prevState) => {
+      return {...prevState, 
+          [inputIdentifier]: +newValue
+      }
+  });
+}
+
+  return (
+    <>
+    <Header></Header>
+    <UserInput onChange = {setInputHandler} userInput={ inputs }></UserInput>
+    {!validInputs && <p>please input a valid duration</p>}
+    {validInputs && <Output userInput = { inputs }></Output> }
+    </>
+  )
+}
+
+export default App
